6e1480af3d72fee3a03624dad8c0aaf133912f84
Jean Carrasco <jean@janium.io>
2024-11-01 04:43:40 +0000

    Commit main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ import ThemeSwitcher from "@/components/ThemeSwitcher/ThemeSwitcher.jsx";
 import DropDown from "@/components/DropDown/DropDown.jsx";
 //ThemeSwitcher
 //-----------------Images------------------------------------
-import myPhoto from "@/components/ProfileHeader/ProfileIcon/mySelf.png";
+import myPhoto from "@/components/ProfileHeader/ProfileIcon/mySelf2.png";
 import slack from "../components/WorkWidget/WorkLogo/slack.png";
 import spotify from "../components/WorkWidget/WorkLogo/spotify.png";
 import microsoft from "../components/WorkWidget/WorkLogo/microsoft.png";
@@ -35,24 +35,23 @@ export default function Home() {
   const articles = [
     {
       date: "December 25, 2023",
-      title: "Lorem ipsum dolor sit amet",
+      title: "Chief Operations Officer at Maderas América",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue...",
+        "As COO of a melamine and construction materials company, I managed daily operations across five warehouses, optimized inventory, and supervised machinery maintenance to ensure continuous production. My role extended to overseeing material importation, coordinating with international suppliers, and adhering to import regulations. This position sharpened my skills in inventory management, machinery upkeep, and international trade.",
       link: "https://medium.com/the-research-nest/everything-you-need-to-know-about-react-ab24da4275ea",
     },
-
     {
       date: "December 25, 2023",
-      title: "Lorem ipsum dolor sit amet",
+      title: "General Manager at HIDROALEMANA S.A.C.",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue...",
+        "As the Administrator of a hydraulic well solutions company, I managed submersible pump sales, coordinated projects for clients like Alicorp and Nestlé, and ensured precise well management systems. Responsibilities included financial oversight, contract negotiations, and safety compliance, strengthening my skills in strategic management and client relations.",
       link: "https://medium.com/javascript-scene/the-missing-introduction-to-react-62837cb2fd76",
     },
     {
       date: "December 25, 2023",
-      title: "Lorem ipsum dolor sit amet",
+      title: "Administrative Assistant at Inmobiliaria Edifica",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue...",
+        "In this real estate role, I efficiently handled client inquiries, coordinated property viewings, and maintained detailed records. I managed essential paperwork and supported agents with meetings and correspondence, honing my multitasking, communication, and problem-solving skills.",
       link: "https://medium.com/javascript-scene/the-missing-introduction-to-react-62837cb2fd76",
     },
   ];
@@ -156,8 +155,27 @@ export default function Home() {
           {/*End of Navbar First part*/}
           <div className="left-inside-144">
             <ProfileHeader
-              name="Software engineer, father, and believer"
-              bio="Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi.."
+              name="Software Engineer | Administrator | Project Engineer | MBA | IB Consultant"
+              bio={
+                <>
+                  <span style={{ display: "block", marginBottom: "1em" }}>
+                    Chief of Operations and seasoned Industrial Engineer with a robust background in DevOps and
+                    administrative leadership, I bring a dynamic blend of expertise to the table. My career is marked by
+                    substantial experience in operational management, continuous improvement, and process optimization.
+                  </span>
+                  <span style={{ display: "block", marginBottom: "1em" }}>
+                    As a Project Engineer, I have honed my skills in seamlessly integrating project management
+                    principles with technical acumen, ensuring the successful delivery of complex projects. My MBA
+                    education has further enriched my strategic and analytical abilities, enabling me to make
+                    data-driven decisions that propel business growth.
+                  </span>
+                  <span style={{ display: "block", marginBottom: "1em" }}>
+                    In the realm of international business, my role as an IB Consultant has allowed me to navigate
+                    diverse business environments, fostering a global perspective in my approach to operations and
+                    strategy.
+                  </span>
+                </>
+              }
               linkedinUrl="https://www.linkedin.com/in/jean-carrasco/"
               imageUrl={myPhoto}
               items={FollowLinks}
@@ -186,7 +204,9 @@ export default function Home() {
               {/*End of SignupWidget*/}
               <WorkWidget
                 title={"Work"}
-                content={"Industrial - Software Engineer with more than seven years of experience optimizing industrial processes with technology and applications."}
+                content={
+                  "Industrial - Software Engineer with more than seven years of experience optimizing industrial processes with technology and applications."
+                }
                 experiences={myExperience}
               />
               {/*End of WorkWidget*/}
